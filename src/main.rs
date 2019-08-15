e57af915f0d6e9f7ab3c591ad31a225a049bf2bc
Steve Sloan <steve@stevesloan.ca>
2019-08-15 15:04:55 -0400

    main.rs: test returning json

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
 
+use rocket::response::content;
+use rocket_contrib::json::Json;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> content::Json<&'static str> {
+    content::Json("{ 'hi': 'world' }")
 }
 
 fn main() {
