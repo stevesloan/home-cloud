71c7f26780f46b1a632bafd077be5f9e7446b9cb
Steve Sloan <steve@stevesloan.ca>
2019-09-02 14:54:04 -0400

    Add surveilence route. test fetching data from server

---
+++ client/src/components/Surveillance.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap';
+
+
+
+
+
+const Surveillence = () => (
+  <div>
+    Surveillance
+  </div>
+)
+
+export default Surveillence
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router'
 import Home from '../components/Home'
 import Hello from '../components/Hello'
-import Surveillance from '../components/Hello'
+import Surveillance from '../components/Surveillance'
 import NoMatch from '../components/NoMatch'
 import NavBar from '../components/NavBar'
 import Container from 'react-bootstrap/Container';
