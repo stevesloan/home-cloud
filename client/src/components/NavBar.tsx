8ddf3c21dcc220686429a2f699c2baabdcecafdb
Steve Sloan <steve@stevesloan.ca>
2019-10-05 17:08:00 -0400

    Client navbar: add state to nav to show different items based on auth

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -3,36 +3,56 @@ import { Nav, Navbar, Container, NavItem } from 'react-bootstrap';
 import { application } from '../config';
 import { LinkContainer } from 'react-router-bootstrap';
 import './NavBar.scss'
+import { connect } from 'react-redux';
+import { AccessToken } from '../store/auth/reducers'
+import { State as RootState } from '../store'
 
-const NavBar = () => (
-  <Navbar bg="dark" variant="dark" expand="lg">
+const NavBar = (props: StateProps) => {
+  console.log('navProps', props)
+  return (<Navbar bg="dark" variant="dark" expand="lg">
     <Container>
       <Navbar.Brand><LinkContainer to="/"><NavItem>{application.name}</NavItem></LinkContainer></Navbar.Brand>
-      <Nav>
-        <LinkContainer to="/">
-          <NavItem>Home</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/hello">
-          <NavItem>Hello</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/surveillance">
-          <NavItem>Surveillance</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/login">
-          <NavItem>Login</NavItem>
-        </LinkContainer>
-        {/* <Nav.Item>
-          <Nav.Link to="/"><Link to="/">Home</Link></Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link to="/hello" eventKey="link-1"><Link to="/hello">Hello</Link></Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link to="/surveillance" eventKey="link-1"><Link to="/surveillance">Surveillance</Link></Nav.Link>
-        </Nav.Item> */}
-      </Nav>
-    </Container>
-  </Navbar>
-)
 
-export default NavBar
+      {props.auth.accessToken ? (
+        <Nav>
+          <LinkContainer to="/">
+            <NavItem>Home</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/hello">
+            <NavItem>Hello</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/surveillance">
+            <NavItem>Surveillance</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/profile">
+            <NavItem>{props.auth.username}</NavItem>
+          </LinkContainer>
+        </Nav>
+      ) : (
+          <Nav>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        )
+      }
+
+    </Container>
+  </Navbar>);
+}
+
+
+
+interface StateProps {
+  auth: AccessToken
+}
+
+const mapStateToProps = (state: RootState): StateProps => {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(NavBar);
